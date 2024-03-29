b6afc0971659f61c56865aed4836701ea9e0050e
Chafik <chafik.hnini@gmail.com>
2024-03-29 11:42:46 +0100

    fix(http-server-go): adjust json file service implementation (#13)

--- impl/http-server-go/go.mod
+++ impl/http-server-go/go.mod
@@ -12,4 +12,10 @@ require (
 	github.com/mackerelio/go-osstat v0.2.4 // direct
 )
 
-require golang.org/x/sys v0.18.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ impl/http-server-go/service_file_json_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"openapi"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindItems(t *testing.T) {
+	// Given
+	filePath := "../../data/servers.example.json"
+
+	// When
+	items, err := findItems(&filePath)
+
+	// Then
+	assert.Nil(t, err)
+	assert.Len(t, items, 2)
+	item := items[0]
+	assert.Equal(t, "medium", *item.Flavor.Get())
+	assert.Equal(t, "server01.mycompany.com", *item.Fqdn.Get())
+	assert.Equal(t, "123", item.Id)
+	assert.Len(t, item.Metrics, 2)
+	assert.Equal(t, "server01", item.Name)
+	assert.Equal(t, "Paris 01", item.Scopes.Geo.Get().Label)
+	assert.Equal(t, "Project 1", item.Scopes.Logical.Get().Label)
+	assert.Equal(t, int32(22), item.Ssh.Get().Port)
+	assert.Equal(t, "nodejs", *item.Stack.Get())
+	assert.Equal(t, openapi.OFF, item.Status)
+}
